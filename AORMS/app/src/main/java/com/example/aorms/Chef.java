e59d1989935994061b7bacd199785282fa305ad0
Muhammad Abdullah Aziz <45502038+MuhammadAbdullahAziz98@users.noreply.github.com>
2019-11-26 22:38:09 +0500

    Changes to chef.java

--- AORMS/app/src/main/java/com/example/aorms/Chef.java
+++ AORMS/app/src/main/java/com/example/aorms/Chef.java
@@ -1,19 +1,49 @@
 package com.example.aorms;
 
+import java.util.ArrayList;
+
 public class Chef implements java.io.Serializable{
+
     String name;
     String specialty;
-    public Chef(String n, String s){
-        this.name = n;
-        this.specialty=s;
-    }
+    int threshold;
+    int currentWorkload;
 
+    public Chef(String name, String specialty, int threshold, int currentWorkload) {
+        this.name = name;
+        this.specialty = specialty;
+        this.threshold = threshold;
+        this.currentWorkload = currentWorkload;
+    }
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String getSpecialty() {
         return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public int getThreshold() {
+        return threshold;
+    }
+
+    public void setThreshold(int threshold) {
+        this.threshold = threshold;
+    }
+
+    public int getCurrentWorkload() {
+        return currentWorkload;
+    }
+
+    public void setCurrentWorkload(int currentWorkload) {
+        this.currentWorkload = currentWorkload;
     }
 }
