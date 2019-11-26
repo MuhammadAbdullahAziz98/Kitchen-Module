be4e2486f61330dfd27069e039205edf47db2a09
Muhammad Abdullah Aziz <45502038+MuhammadAbdullahAziz98@users.noreply.github.com>
2019-11-26 22:33:31 +0500

    Order schema change

---
+++ AORMS/app/src/main/java/com/example/aorms/OrderModel .java
@@ -0,0 +1,83 @@
+package com.example.aorms;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.List;
+@IgnoreExtraProperties
+public class OrderModel {
+    int order_id;
+    String status;
+    int table_id;
+    Timestamp order_prep_time;
+    float bill;
+    List<OrderDishInfoModel> OrderPlaced;
+
+    public OrderModel(int order_id, Timestamp ordertime, String status, int table_id, Timestamp order_prep_time, float bill, List<OrderDishInfoModel> orderPlaced) {
+        this.order_id = order_id;
+        this.ordertime = ordertime;
+        this.status = status;
+        this.table_id = table_id;
+        this.order_prep_time = order_prep_time;
+        this.bill = bill;
+        OrderPlaced = orderPlaced;
+    }
+
+    public int getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(int order_id) {
+        this.order_id = order_id;
+    }
+
+    public Timestamp getOrdertime() {
+        return ordertime;
+    }
+
+    public void setOrdertime(Timestamp ordertime) {
+        this.ordertime = ordertime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getTable_id() {
+        return table_id;
+    }
+
+    public void setTable_id(int table_id) {
+        this.table_id = table_id;
+    }
+
+    public Timestamp getOrder_prep_time() {
+        return order_prep_time;
+    }
+
+    public void setOrder_prep_time(Timestamp order_prep_time) {
+        this.order_prep_time = order_prep_time;
+    }
+
+    public float getBill() {
+        return bill;
+    }
+
+    public void setBill(float bill) {
+        this.bill = bill;
+    }
+
+    public List<OrderDishInfoModel> getOrderPlaced() {
+        return OrderPlaced;
+    }
+
+    public void setOrderPlaced(List<OrderDishInfoModel> orderPlaced) {
+        OrderPlaced = orderPlaced;
+    }
+}
+
